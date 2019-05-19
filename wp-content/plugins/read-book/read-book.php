a9e2956889f48edf8955a4e0b7d69941de205d00
rdetobel <rdetobel@ulb.ac.be>
2019-05-19 21:11:59 +0200

    Extend research to take into account custom field

--- wp-content/plugins/read-book/read-book.php
+++ wp-content/plugins/read-book/read-book.php
@@ -209,7 +209,55 @@ function wpc_cpt_in_search($query) {
 }
 add_action('pre_get_posts','wpc_cpt_in_search');
 
+function cf_search_join( $join ) {
+    global $wpdb;
 
+    if ( is_search() ) {
+        $join .=' LEFT JOIN '.$wpdb->postmeta. ' ON '. $wpdb->posts . '.ID = ' . $wpdb->postmeta . '.post_id ';
+    }
+
+    return $join;
+}
+add_filter('posts_join', 'cf_search_join');
+
+/**
+ * Modify the search query with posts_where
+ *
+ * http://codex.wordpress.org/Plugin_API/Filter_Reference/posts_where
+ */
+function cf_search_where( $where ) {
+    global $pagenow, $wpdb;
+
+    if ( is_search() ) {
+        $where = preg_replace(
+            "/\(\s*".$wpdb->posts.".post_title\s+LIKE\s*(\'[^\']+\')\s*\)/",
+            "(".$wpdb->posts.".post_title LIKE $1) OR (".$wpdb->postmeta.".meta_value LIKE $1)", $where );
+    }
+
+    return $where;
+}
+add_filter('posts_where', 'cf_search_where');
+
+/**
+ * Prevent duplicates
+ *
+ * http://codex.wordpress.org/Plugin_API/Filter_Reference/posts_distinct
+ */
+function cf_search_distinct( $where ) {
+    global $wpdb;
+
+    if ( is_search() ) {
+        return "DISTINCT";
+    }
+
+    return $where;
+}
+add_filter('posts_distinct', 'cf_search_distinct');
+
+
+
+
+///////////////// SUBMIT /////////////////
 
 function submit_document_point_callback($form_data) {
     global $ninja_forms_processing;
